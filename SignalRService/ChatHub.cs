5be8a9f537394c90e97455493bd60709e9a8981f
Roma <rkurenok99@gmail.com>
2022-04-10 21:59:50 +0300

    Добавьте файлы проекта.

---
+++ SignalRService/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRService
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string username, string message, string time)
+        {
+            await this.Clients.All.SendAsync("Receive", username, message, time);
+        }
+    }
+}
